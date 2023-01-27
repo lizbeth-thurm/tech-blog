93db0bc1b759ec09ffd71f59760734c42140e221
slthurman01 <slthurman01@gmail.com>
2023-01-27 11:56:18 -0600

    Adds files to seeds folder and models folder.  Adds code to home-routes.

---
+++ controllers/home-routes.js
@@ -0,0 +1,26 @@
+// Navigates pages
+const express = require("express");
+const router = express.Router();
+const { Post } = require("../models");
+
+//home page
+router.get("/", function (req, res) {
+    Post.findAll().then((postData) => {
+        let posts = postData.map((m) => m.get({ plain: true }));
+        res.render("homepage", { posts })
+    }).catch((err) => {
+        res.status(500).json({ "error": err });
+    })
+});
+
+//login page
+router.get('/login', (req, res) => {
+    res.render("login")
+})
+
+//dashboard
+router.get('/dashboard', (req, res) => {
+    res.render("dashboard")
+})
+
+module.exports = router;
---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+// Connects home routes and api routes
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
