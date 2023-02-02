c5572550130d980a32ad3b57107fae46653e5277
slthurman01 <slthurman01@gmail.com>
2023-02-02 11:17:40 -0600

    Adds js for signup

---
+++ public/js/signup.js
@@ -0,0 +1,24 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const emailNew = document.querySelector('#email-signup');
+    const passwordNew = document.querySelector('#password-signup');
+
+    const response = await fetch('/api/user/signup', {
+        method: 'POST',
+        body: JSON.stringify({
+            email: emailNew.value,
+            password: passwordNew.value,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Unable to signup');
+    }
+};
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
