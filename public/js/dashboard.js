1988e712b5a12db8a44e48098b032323ba02efea
slthurman01 <slthurman01@gmail.com>
2023-02-02 11:22:58 -0600

    Adds js for creating, deleting, and updating posts.

---
+++ public/js/dashboard.js
@@ -0,0 +1,71 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#post-title').value.trim();
+    const text = document.querySelector('#post-text').value.trim();
+  
+    // create posts
+    if (title && text) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, text }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create post');
+      }
+    }
+  };
+
+
+  //delete posts
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+  };
+
+  // update posts
+    const updateButtonHandler = async (event) => {
+        if (event.target.hasAttribute('data-id')) {
+            const id = event.target.getAttribute('data-id');
+            const title = document.querySelector('#post-title').value.trim();
+            const text = document.querySelector('#post-text').value.trim();
+            const response = await fetch(`/api/posts/${id}`, {
+                method: 'PUT',
+                body: JSON.stringify({ title, text }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                document.location.replace('/dashboard');
+            } else {
+                alert('Failed to update post');
+            }
+        }
+    };
+ 
+  document
+    .querySelector('.new-post-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.post-list')
+    .addEventListener('click', delButtonHandler);
+  
