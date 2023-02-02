3d19b7dd1583d8f70696a32f1ccb898844fbb725
slthurman01 <slthurman01@gmail.com>
2023-02-02 08:38:43 -0600

    Adds post seed

---
+++ seeds/postData.js
@@ -0,0 +1,21 @@
+const { Post } = require('../models');
+
+const seedPost = () => Post.bulkCreate([
+    {
+        "title": "Turtles",
+        "text": "Hey does anybody else like turtles?",
+        "user_id": 1
+    },
+    {
+        "title": "Lizards",
+        "text": "Lizards are my favorite reptiles",
+        "user_id": 2
+    },
+    {
+        "title": "Birds",
+        "text": "Cladistically, what's the difference between birds and reptiles?",
+        "user_id": 3
+    }
+]);
+
+module.exports = seedPost;
