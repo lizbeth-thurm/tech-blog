36b67f506ff1c621c7622265481ef593773dd4c8
slthurman01 <slthurman01@gmail.com>
2023-02-02 08:22:27 -0600

    adds routes to add and delete posts.

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,39 @@
+// routes for adding and retrieving post data
+const router = require('express').Router();
+
+const Post = require('../../models/Posts');
+
+// create post
+router.post('/', async (req, res) => {
+    try {
+        const postData = await Post.create({
+            ...req.body,
+    user_id: req.session.user_id,
+        });
+        res.status(200).json(newPost);
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// delete post
+router.delete('/:id', async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
